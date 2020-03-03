ef9cc1ebe3cd3b3550c3aa536d3662e02be8e865
WWWNASTY <nas.grischenk@yandex.ru>
2020-03-04 00:01:33 +0300

    Fixed for format of function.

--- BusinessLogicLayer/BusinessLogicLayer.Implementation/Services/TelegramSettings.cs
+++ BusinessLogicLayer/BusinessLogicLayer.Implementation/Services/TelegramSettings.cs
@@ -1,11 +1,11 @@
 namespace BusinessLogicLayer.Implementation.Services
 {
-    public class TelegramSettings //http://telegram.me/ExchangeGoBot
+    public class TelegramSettings //https://core.telegram.org/bots/api
     {
         public static string Url { get; set; }  = "http://telegram.me/ExchangeGoBot";
 
         public static string Name { get; set; } = "ExchangeGoBot";
 
-        public static string Key { get; set; }  = "913603662:AAGiAzwKvZT5v_4bUCLSpxgR5lYI3lqbooE";
+        public static string Key { get; set; }  = "1098322872:AAG4J-iuIF0K9cSiObVjbPelHKshePX0CXo";
     }
 }
